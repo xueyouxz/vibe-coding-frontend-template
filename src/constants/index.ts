5c04f6c39323822017636ce99f1df4d8a89f02ce
zhangxueyou <xueyouxz@163.com>
2025-11-10 15:19:53 +0800

    init

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,3 +12,4 @@ export const ROUTES = {
   USER: '/user',
 } as const
 
+
--- src/store/hooks.ts
+++ src/store/hooks.ts
@@ -5,3 +5,4 @@ import type { RootState, AppDispatch } from './index'
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
+
